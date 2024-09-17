874d74ce5ee7e3b72dd01739504d4abe41d9f807
mcristobal <mcristobaln@outlook.com>
2024-09-17 00:38:17 -0500

    feat: Change name file routes api and configure parametes database

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
-
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::post('/login',[AuthController::class,'show'])->name('api.v1.show');
+Route::middleware('auth:sanctum')->group(function(){
+    Route::post('/register',[AuthController::class,'store'])->name('api.v1.store');
+    Route::post('/logout',[AuthController::class,'destroy'])->name('api.v1.destroy');
 });
