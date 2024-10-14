f8816edd24f76baac869bd7ea953beac424b3552
MANUEL CRISTOBAL NEYRA S - SGIS-05 <MCRISTOBALN@HMPP.COM>
2024-10-14 12:55:42 -0500

    feat: Adding page dashboard

---
+++ resources/views/components/navs/main.blade.php
@@ -0,0 +1,159 @@
+<div class="logo"><a href="http://www.creative-tim.com" class="simple-text logo-mini">
+            {{env('APP_ABBR')}}
+        </a>
+        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+            {{env('APP_COMP')}}
+        </a></div>
+      <div class="sidebar-wrapper">
+            <div class="user">
+            <div class="photo">
+                <img src="../assets/img/faces/avatar.jpg" />
+            </div>
+            <div class="user-info">
+                <a data-toggle="collapse" href="#collapseExample" class="username">
+                <span>
+                    Tania Andrew
+                    <b class="caret"></b>
+                </span>
+                </a>
+                <div class="collapse" id="collapseExample">
+                <ul class="nav">
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <span class="sidebar-mini"> MP </span>
+                        <span class="sidebar-normal"> My Profile </span>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <span class="sidebar-mini"> EP </span>
+                        <span class="sidebar-normal"> Edit Profile </span>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <span class="sidebar-mini"> S </span>
+                        <span class="sidebar-normal"> Settings </span>
+                    </a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+            </div>
+            <ul class="nav">
+                <li class="nav-item active ">
+                    <a class="nav-link" href="../examples/dashboard.html">
+                    <i class="material-icons">dashboard</i>
+                    <p> Dashboard </p>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" data-toggle="collapse" href="#pagesExamples">
+                    <i class="material-icons">image</i>
+                    <p> Pages
+                        <b class="caret"></b>
+                    </p>
+                    </a>
+                    <div class="collapse" id="pagesExamples">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                        <a class="nav-link" href="../examples/pages/pricing.html">
+                            <span class="sidebar-mini"> P </span>
+                            <span class="sidebar-normal"> Pricing </span>
+                        </a>
+                        </li>
+                    </ul>
+                    </div>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" data-toggle="collapse" href="#2">
+                    <i class="material-icons">apps</i>
+                    <p> Componetes
+                        <b class="caret"></b>
+                    </p>
+                    </a>
+                    <div class="collapse" id="2">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                        <a class="nav-link" href="../examples/pages/pricing.html">
+                            <span class="sidebar-mini"> P </span>
+                            <span class="sidebar-normal"> Pricing </span>
+                        </a>
+                        </li>
+                    </ul>
+                    </div>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" data-toggle="collapse" href="#3">
+                    <i class="material-icons">content_paste</i>
+                    <p> forms
+                        <b class="caret"></b>
+                    </p>
+                    </a>
+                    <div class="collapse" id="3">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                        <a class="nav-link" href="../examples/pages/pricing.html">
+                            <span class="sidebar-mini"> P </span>
+                            <span class="sidebar-normal"> Pricing </span>
+                        </a>
+                        </li>
+                    </ul>
+                    </div>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" data-toggle="collapse" href="#4">
+                    <i class="material-icons">grid_on</i>
+                    <p> Tables
+                        <b class="caret"></b>
+                    </p>
+                    </a>
+                    <div class="collapse" id="4">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                        <a class="nav-link" href="../examples/pages/pricing.html">
+                            <span class="sidebar-mini"> P </span>
+                            <span class="sidebar-normal"> Pricing </span>
+                        </a>
+                        </li>
+                    </ul>
+                    </div>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" data-toggle="collapse" href="#5">
+                    <i class="material-icons">place</i>
+                    <p> Maps
+                        <b class="caret"></b>
+                    </p>
+                    </a>
+                    <div class="collapse" id="5">
+                    <ul class="nav">
+                        <li class="nav-item ">
+                        <a class="nav-link" href="../examples/pages/pricing.html">
+                            <span class="sidebar-mini"> P </span>
+                            <span class="sidebar-normal"> Pricing </span>
+                        </a>
+                        </li>
+                    </ul>
+                    </div>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="../examples/widgets.html">
+                    <i class="material-icons">widgets</i>
+                    <p> Widgets </p>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="../examples/charts.html">
+                    <i class="material-icons">timeline</i>
+                    <p> Charts </p>
+                    </a>
+                </li>
+                <li class="nav-item ">
+                    <a class="nav-link" href="../examples/calendar.html">
+                    <i class="material-icons">date_range</i>
+                    <p> Calendar </p>
+                    </a>
+                </li>
+            </ul>
+      </div>
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 }); */
 Route::get('/', function () {
-    return view('authtenticate.login');
+    return view('dashboard.dashboard');
 }); 
 
 
